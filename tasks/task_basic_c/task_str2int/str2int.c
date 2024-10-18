efbe6f6ddc442ac9a0c16376103c95d554ce61c7
Asonov Sergey <asonov007@icloud.com>
2024-10-18 00:04:13 +0000

    all tests are done

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -3,11 +3,11 @@
 #include "stdio.h"
 
 int str2int(const char *str) {
-    int sign = 1;
+    int znak = 1;
     int result = 0;
 
     if  (*str == '-'){
-        sign = -1;
+        znak = -1;
         str++;
     } else if (*str == '+'){
         str++;
@@ -18,9 +18,9 @@ int str2int(const char *str) {
         result = result * 10 +(*str - '0');
         count +=1;
         str++;
-        assert(sign == -1 && result == -2147483648 || sign == -1 && result >= 0 && count <= 9 || sign == 1 && result >= 0);
+        assert(znak  == -1 && result == -2147483648 || znak  == -1 && result >= 0 && count <= 9 || znak == 1 && result >= 0);
 
     }
     assert(count>0);
-    return result * sign;
+    return result * znak;
 }
